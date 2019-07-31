0276ca1feb15ccb7314a107a82568345924d2583
Ulrik Mikaelsson <ulrik.mikaelsson@gmail.com>
2019-07-31 21:09:47 +0200

    src/table: Use C++ iostream and string to render bar more succinctly

--- src/table.cpp
+++ src/table.cpp
@@ -23,20 +23,7 @@ void printTable(int level[])
 
     for(int i = 0; i < 20; i++)
     {
-        if(i < 9)
-        {
-            cout << (i + 1) << "      [";
-        }
-        else
-        {
-            cout << (i + 1) << "     [";
-        }
-
-        for(int k = 0; k < round(level[i] / step); k++)
-        {
-            cout << "=";
-        }
-
-        cout << endl;
+        auto bar = std::string(round(level[i] / step), '=');
+        cout << setw(8) << (i + 1) << "[" << bar << endl;
     }
 }
